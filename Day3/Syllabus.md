d4cc08bac2080d354df74e97ab11099ef457abbb
ArbinBhasimaOfficial <arbinbhasimaofficial@gmail.com>
2026-07-23 09:13:23 +0545

    Changes made on syllabus.md and Day 3 added js started

---
+++ Day3/Syllabus.md
@@ -0,0 +1,49 @@
+JavaScript Fundamentals.
+
+
+  Executable
+
+  Runtime
+
+  Memory Management
+
+
+  Threads
+
+    multiplethreads
+
+  Asynchronous Part
+
+    Async
+
+```
+What is JS? How does it run?
+
+```
+
+Framework
+  The prime time - Primegen
+
+Conditional Statement.
+  Feature
+
+  If - Statement
+  variables
+  loops
+
+
+know what js is know what ts is.
+
+js differ to ts
+
+js allows let x = string
+x = numbers
+
+but ts dont it is strictly type version of js.
+
+electron
+react-native
+
+// rock paper scissors.
+
+defer loads html first and then js file.
---
+++ Day3/script.js
@@ -0,0 +1,58 @@
+// statement
+console.log("Hajimemasite!")
+
+// for statement
+// for(initialize;condition;increment;){
+
+// }
+// if statement(){
+//   ..
+// }
+
+
+// expression
+let x = 5  + 2
+
+// boolean expression
+let y = 5 < 10
+
+// left sheft operation
+let z = 5 == 5
+
+// Variables
+
+let name = "Arbin"
+// reassign value used in let
+
+var age = 23
+
+
+const isStudent = false
+// can't assign value because it is constant.
+
+const obj = {
+  name: "Arbin"
+}
+
+
+// primitive Data types
+// null
+// undefined
+
+// variable declared and null value is assigned
+let nulvar = null
+
+// variable declared value is not assigned
+let undefinedVar;
+
+// variable declared and value is assigned but is undefined
+let undefinedVar2 = undefined
+
+// [symbol]
+// unique identifier
+
+// string
+
+// complex data types
+
+// task try to build rock paper scissors
