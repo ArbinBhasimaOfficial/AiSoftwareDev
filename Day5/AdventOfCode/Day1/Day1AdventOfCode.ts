31d814a226caeda8c9c2d9813a7c0ddfbdc5ada8
ArbinBhasimaOfficial <arbinbhasimaofficial@gmail.com>
2026-07-28 14:42:29 +0545

    Added Day 6 and Day 5 AdventOfCode Day 1 Part one and two done.

--- Day5/AdventOfCode/Day1/Day1AdventOfCode.ts
+++ Day5/AdventOfCode/Day1/Day1AdventOfCode.ts
@@ -2,6 +2,7 @@ import fs from "fs/promises"
 
 const Start = 50
 const max = 100
+const wheel = Array.from({length : 100})
 
 
 function zionPartOne(rotations: string[]){
@@ -12,9 +13,9 @@ function zionPartOne(rotations: string[]){
         const value = Number(rotations.slice(1))
         // console.log({direction, value})
         if (direction === "R"){
-            current = (current + value) % max 
+            current = (current + value) % max
         } else if (direction === "L") {
-            current = (current - value) % max 
+            current = (current - value) % max
             if (current < 0){
                 current = max + current
             }
@@ -28,36 +29,39 @@ function zionPartOne(rotations: string[]){
 
     // 979 too low
     console.log(password)
-    // 1165 is the correct answer 
+    // 1165 is the correct answer
 }
 
-function zionPartTwo(rotations: string[]){
-    let current = Start
-    let password = 0
-    rotations.forEach((rotations) => {
-        const direction = rotations.slice(0, 1)
-        const value = Number(rotations.slice(1))
-        console.log({direction, value})
-        if (direction === "R"){
-            current = (current + value) % max 
-        } else if (direction === "L") {
-            current = (current - value) % max 
-            if (current < 0){
-                current = max + current
-            }
-        }
-
-        if(current === 0){
-            password += 1
-        }
-
-    })
-    console.log(password)
-}
-
+// function zionPartTwo(rotations: string[]){
+//   let current = start
+//   let password = 0
+//   rotations.forEach((rotation) => {
+//     const direction = rotation.slice(0,1)
+//     const value = Number(rotation.slice(1))
+//     console.log({direction, value})
+//     if (direction === "R"){
+//       for (let i = current + 1; i <= current + value; i++){
+//         if (WHEEL[i % WHEEl.length] === 0){
+//           password ++
+//         }
+//       }
+//       current = (current + value)&max
+//     } else if (direction === "L"){
+//       for(let i = current - 1; i >= current - value; i--){
+//         if(WHEEL[i % WHEEL.length] === 0){
+//           password ++
+//         }
+//       }
+//       current = (current - value) % max
+//       if(current < 0){
+//         current = max + current
+//       }
+//     }
+//   })
+// }
 const input = await fs.readFile("../input.txt", "utf-8")
 
 
 // zionPartOne(input.split("\n"))
 
-zionPartTwo(input.split("\n"))
+zionPartOne(input.split("\n"))
